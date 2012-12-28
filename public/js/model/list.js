5035d0e41b00f3805a61011eb0c801086b0171a2
mikemoraned <mike@houseofmoran.com>
2012-12-28 20:26:33 +0100

    added shell, not yet saving

--- public/js/model/list.js
+++ public/js/model/list.js
@@ -1,4 +1,14 @@
 (function(ko, Trello, global) {
+    function MoveToTop(listId, cardId) {
+        this.listId = listId;
+        this.cardId = cardId;
+    }
+
+    MoveToTop.prototype.do = function (success, failure) {
+        console.log("Moving " + this.cardId + " to top of " + this.listId);
+        success();
+    }
+
     function List(data) {
         var self = this;
         self.id = ko.observable(data.id);
@@ -24,9 +34,30 @@
                 console.log("Remaining:");
                 console.dir(remaining);
 
+                selected.forEach(function(c) {
+                    self.pendingActions.push(new MoveToTop(self.id(), c.id()));
+                });
+
                 self.cards(selected.concat(remaining));
             }
         }
+
+        self.pendingActions = ko.observableArray([]);
+
+        self.hasPendingActions = ko.computed(function() {
+            return self.pendingActions().length > 0;
+        });
+
+        self.doPendingActions = function() {
+            console.log("Doing pending actions");
+            self.pendingActions().forEach(function(action) {
+                action.do(self.refresh, function(error) {
+                    console.log("Error:");
+                    console.dir(error);
+                });
+            });
+            self.pendingActions([]);
+        };
 
         self.refresh = function() {
             Trello.lists.get(self.id() + "/cards", function(results) {
