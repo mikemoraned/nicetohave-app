c00c76dc2171df52bb1ecea11dc521523266a6df
mikemoraned <mike@houseofmoran.com>
2013-02-17 19:41:17 +0000

    cache categorisation in app model so that we do not reset them on load

--- public/js/appViewModel2.coffee
+++ public/js/appViewModel2.coffee
@@ -8,8 +8,15 @@ class AppViewModel
       new nicetohave.Card("510557f3e002eb8d56002e04", @privilege),
       new nicetohave.Card("5105af6108fa2a6e21000dc5", @privilege)
     ])
+    @_cachedCategorisations = {};
+
     @categorisations = ko.computed(() =>
-      @cards().map (card) -> new nicetohave.Categorisation(card)
+      @cards().map (card) =>
+        if (@_cachedCategorisations[card.id()])
+          @_cachedCategorisations[card.id()]
+        else
+          @_cachedCategorisations[card.id()] = new nicetohave.Categorisation(card)
+          @_cachedCategorisations[card.id()]
     )
 
 window.nicetohave.AppViewModel = AppViewModel
--- public/js/appViewModel2.js
+++ public/js/appViewModel2.js
@@ -12,9 +12,15 @@
       var _this = this;
       this.privilege = new nicetohave.Privilege(Trello);
       this.cards = ko.observableArray([new nicetohave.Card("510557f3e002eb8d56002e04", this.privilege), new nicetohave.Card("5105af6108fa2a6e21000dc5", this.privilege)]);
+      this._cachedCategorisations = {};
       this.categorisations = ko.computed(function() {
         return _this.cards().map(function(card) {
-          return new nicetohave.Categorisation(card);
+          if (_this._cachedCategorisations[card.id()]) {
+            return _this._cachedCategorisations[card.id()];
+          } else {
+            _this._cachedCategorisations[card.id()] = new nicetohave.Categorisation(card);
+            return _this._cachedCategorisations[card.id()];
+          }
         });
       });
     }
